41430ddeb27824b7d9c90cf8ec84b077d55f9525
v4n0v <avetchinov@gmail.com>
2018-04-22 00:34:56 +0300

    upd

--- app/src/main/java/com/example/v4n0v/wethariumreact/okApi/ConnectionManager.java
+++ app/src/main/java/com/example/v4n0v/wethariumreact/okApi/ConnectionManager.java
@@ -1,19 +1,18 @@
 package com.example.v4n0v.wethariumreact.okApi;
 
+import java.net.URL;
+
 import io.reactivex.Single;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-/**
- * Created by v4n0v on 22.04.18.
- */
-
 public class ConnectionManager {
-
+    private static final String OPEN_API_MAP = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    private static final String OPEN_API_KEY = "1aa546d01134ed09d869b84c7e83e34f";
     private static ConnectionManager instance;
 
     public static ConnectionManager getConnection() {
-        if (instance==null) return new ConnectionManager();
+        if (instance == null) return new ConnectionManager();
         return instance;
     }
 
@@ -21,8 +20,10 @@ public class ConnectionManager {
     public static Single<String> getWeather(String city) {
         return Single.fromCallable(() -> {
             OkHttpClient client = new OkHttpClient();
+            URL url = new URL(String.format(OPEN_API_MAP, city) + "&appid=" + OPEN_API_KEY);
+
             Request req = new Request.Builder()
-                    .url("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=1aa546d01134ed09d869b84c7e83e34f")
+                    .url(url)
                     .build();
             return client.newCall(req).execute().body().string();
         });
