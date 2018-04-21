a6b6b22e331c59c44315e000afd236d8e7053033
v4n0v <avetchinov@gmail.com>
2018-04-21 23:28:16 +0300

    upd

--- app/src/main/java/com/example/v4n0v/wethariumreact/api/ApiService.java
+++ app/src/main/java/com/example/v4n0v/wethariumreact/api/ApiService.java
@@ -10,10 +10,9 @@ import retrofit2.http.Query;
 public interface ApiService {
     // http://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=1aa546d01134ed09d869b84c7e83e34f
 //
-    @GET()
-    Observable<WeatherInfo> getWeather(@Query("appid") String key,
-                                       @Query("q") String city,
-                                       @Query("units") String units);
+    @GET("weather?&units=metric")
+    Observable<WeatherInfo> getWeather(@Query("q") String city,
+                                       @Query("appid") String key);
 //    @GET()
 //    Observable<WeatherInfo> getWeather();
 }
